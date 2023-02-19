1ecde1249dbb8dd0f7f25b5656157dfb42cbb3d5
Jorge Toribio <jorgeluistoribio14@gmail.com>
2023-02-18 21:29:48 -0400

    first commit

---
+++ Poligono.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_Capitulo_10
+{
+    public class Poligono
+    {
+        private int numLados;
+        private double longitudLado;
+
+        public Poligono(int numLados, double longitudLado)
+        {
+            this.numLados = numLados;
+            this.longitudLado = longitudLado;
+        }
+
+        public Poligono(int numLados) : this(numLados, 0.0)
+        {
+        }
+
+        public Poligono() : this(0, 0.0)
+        {
+        }
+    }
+
+}
